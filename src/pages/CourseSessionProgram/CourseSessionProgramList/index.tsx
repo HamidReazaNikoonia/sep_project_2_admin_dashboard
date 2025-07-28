9eb4787b9fe6104b2c3e23a8d5e7b1ab83414ade
hamidrezanikoonia <hamid.nikoonia@ecashpay.com.ph>
2025-07-29 03:23:43 +0330

    update program list

--- src/pages/CourseSessionProgram/CourseSessionProgramList/index.tsx
+++ src/pages/CourseSessionProgram/CourseSessionProgramList/index.tsx
@@ -32,18 +32,59 @@ const ProgramList = () => {
                                 <p className='text-xs text-gray-500' >مربی </p>
                                 <p className="font-medium mb-2">{program?.coach?.first_name} - {program?.coach?.last_name}</p>
                             </div>
+
+                            <div className="flex flex-row items-center mt-1">
+                                <div style={{ maxWidth: '60px' }} className={`px-2 py-1 text-xs text-center rounded-full ${program?.status === 'active' ? 'bg-green-100 text-green-800' :
+                                    program?.status === 'completed' ? 'bg-blue-100 text-blue-800' :
+                                        'bg-red-100 text-red-800'
+                                    }`}>
+                                    {program?.status === 'active' ? 'فعال' :
+                                        program?.status === 'completed' ? 'تکمیل شده' :
+                                            'غیرفعال'}
+                                </div>
+                            </div>
                         </div>
 
                         {/* Row 4: Status + Actions */}
-                        <div className="flex justify-between items-center">
+                        <div className="flex flex-col items-center">
                             <div className="flex space-x-2">
-                                <Link
-                                    to={`/users/${program?._id}/edit`}
-                                    className="px-2 py-1 text-sm text-blue-600 hover:bg-blue-50 rounded"
-                                >
-                                    تغییرات
-                                </Link>
+                                <p className="text-xs text-gray-500">شروع کلاس</p>
+                                <p className="text-xs font-semibold text-gray-700">{findFirstSession(program?.sessions)}</p>
                             </div>
+                            <div className="flex mt-2">
+                                <p className="text-xs flex flex-row items-center">
+                                    <span className="text-xs text-gray-500 ml-3 block">نوع دوره</span>
+                                    {programTypeMap[program.program_type] || 'N/A'}
+                                </p>
+                            </div>
+
+                            <div className="flex flex-row items-center mt-2">
+                                <p className="text-xs text-gray-500 ml-2">تعداد ثبت نام</p>
+                                <p className="text-xs text-gray-700">{program?.members?.length && (program?.members?.length).toLocaleString('fa-IR')} / {program?.max_member_accept?.toLocaleString('fa-IR')}</p>
+                            </div>
+
+                            <div className="flex flex-row items-center mt-4">
+                                <p className="text-xs text-gray-500">قیمت</p>
+                                <div className="flex items-center gap-1">
+                                    {program?.price_discounted ? (
+                                        <div className='flex flex-col'>
+                                            <p className="text-sm font-medium text-gray-700">
+                                                {program.price_discounted.toLocaleString('fa-IR')} ریال
+                                            </p>
+                                            <p className="text-xs text-gray-400 line-through">
+                                                {program.price_real.toLocaleString('fa-IR')}
+                                            </p>
+                                        </div>
+                                    ) : (
+                                        <p className="text-sm text-gray-700">
+                                            {program.price_real.toLocaleString('fa-IR')} ریال
+                                        </p>
+                                    )}
+                                </div>
+                            </div>
+
+
+
                         </div>
                     </div>
 
@@ -82,45 +123,44 @@ const ProgramList = () => {
                     </div>
 
                     <div className="col-span-1">
-                        <div style={{maxWidth: '60px'}} className={`px-2 py-1 text-xs text-center rounded-full ${
-                            program?.status === 'active' ? 'bg-green-100 text-green-800' :
+                        <div style={{ maxWidth: '60px' }} className={`px-2 py-1 text-xs text-center rounded-full ${program?.status === 'active' ? 'bg-green-100 text-green-800' :
                             program?.status === 'completed' ? 'bg-blue-100 text-blue-800' :
-                            'bg-red-100 text-red-800'
-                        }`}>
+                                'bg-red-100 text-red-800'
+                            }`}>
                             {program?.status === 'active' ? 'فعال' :
-                             program?.status === 'completed' ? 'تکمیل شده' :
-                             'غیرفعال'}
+                                program?.status === 'completed' ? 'تکمیل شده' :
+                                    'غیرفعال'}
                         </div>
                     </div>
 
                     <div className="col-span-1">
-                       <p className="text-xs text-gray-700">کلاس</p>
-                       <p className="text-sm text-gray-700">{program?.class_id?.class_title}</p>
+                        <p className="text-xs text-gray-700">کلاس</p>
+                        <p className="text-sm text-gray-700">{program?.class_id?.class_title}</p>
                     </div>
 
                     <div className="col-span-1">
-                       <p className="text-xs text-gray-500">شروع کلاس</p>
-                       <p className="text-sm text-gray-700">{findFirstSession(program?.sessions)}</p>
+                        <p className="text-xs text-gray-500">شروع کلاس</p>
+                        <p className="text-sm text-gray-700">{findFirstSession(program?.sessions)}</p>
                     </div>
 
                     <div className="col-span-1">
-                       <p className="text-xs text-gray-500">قیمت</p>
-                       <div className="flex items-center gap-1">
-                         {program?.price_discounted ? (
-                           <div className='flex flex-col'>
-                             <p className="text-sm font-medium text-gray-700">
-                               {program.price_discounted.toLocaleString('fa-IR')} ریال
-                             </p>
-                             <p className="text-xs text-gray-400 line-through">
-                               {program.price_real.toLocaleString('fa-IR')}
-                             </p>
-                           </div>
-                         ) : (
-                           <p className="text-sm text-gray-700">
-                             {program.price_real.toLocaleString('fa-IR')} ریال
-                           </p>
-                         )}
-                       </div>
+                        <p className="text-xs text-gray-500">قیمت</p>
+                        <div className="flex items-center gap-1">
+                            {program?.price_discounted ? (
+                                <div className='flex flex-col'>
+                                    <p className="text-sm font-medium text-gray-700">
+                                        {program.price_discounted.toLocaleString('fa-IR')} ریال
+                                    </p>
+                                    <p className="text-xs text-gray-400 line-through">
+                                        {program.price_real.toLocaleString('fa-IR')}
+                                    </p>
+                                </div>
+                            ) : (
+                                <p className="text-sm text-gray-700">
+                                    {program.price_real.toLocaleString('fa-IR')} ریال
+                                </p>
+                            )}
+                        </div>
                     </div>
                 </div>
             </Link>
